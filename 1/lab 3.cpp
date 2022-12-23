80efb9c8e0f1db69e90a71023ca8e86868f314d8
Nver1302 <118624204+Nver1302@users.noreply.github.com>
2022-12-23 18:38:23 +0400

    Create lab 3.cpp

---
+++ 1/lab 3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+string arr1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+string arr2 = "ZABCDEFGHIGKLMNOPQRSTUVWXYzabcdefghijklmnopqrstuvwxy1234567890";
+
+string ncoder(string str) {
+	int x;
+	int i = 0;
+	while (str[i]) {
+		x = 0;
+		while (arr2[x]) {
+			if (str[i] == arr2[x]) {
+				str[i] = arr1[x];
+				break;
+			}
+			x++;
+		}
+		i++;
+	}
+	return str;
+}
+
+string dcoder(string str) {
+	int x;
+	int i = 0;
+	while (str[i]) {
+		x = 0;
+		while (arr1[x]) {
+			if (str[i] == arr1[x]) {
+				str[i] = arr2[x];
+				break;
+			}
+			x++;
+		}
+		i++;
+	}
+	return str;
+}
+
+int main() {
+	string str = "A big cat - 12";
+	string decodedStr = dcoder(str);
+	string old = ncoder(decodedStr);
+	cout << decodedStr << endl << old;
+
+}
